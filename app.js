292f83b3fe4f0b4fd9a8e2943737f642bc524829
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-20 22:17:32 -0400

    refactor bookRouter into its own file

--- app.js
+++ app.js
@@ -13,41 +13,8 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
+var bookRouter = require('./routes/bookRoutes')();
 
-bookRouter.route('/Books')
-  .post(function(req, res) {
-    var book = new Book(req.body);
-
-    book.save();
-    res.status(201).send(book);
-
-  })
-  .get(function(req, res) {
-    var query = {};
-
-    if(req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, function(err, books) {
-      if(err) {
-        res.status(500).send(err);
-      } else {
-        res.json(books);
-      }
-    });
-  });
-bookRouter.route('/books/:bookId')
-.get(function(req, res) {
-
-  Book.findById(req.params.bookId, function(err, book) {
-    if(err) {
-      res.status(500).send(err);
-    } else {
-      res.json(book);
-    }
-  });
-});
 app.use('/api', bookRouter);
 
 app.get('/', function(req, res) {
